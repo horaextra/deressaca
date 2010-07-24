6d6d4306d7db81cffd21d2523e84021953c5e87c
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-24 20:26:00 -0300

    Corrige o acesso aos arquivos estáticos

--- src/urls.py
+++ src/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from ressaca.shortcuts import route
 from ressaca.views import show_counter, inc_hangover_counter
 
 urlpatterns = patterns('',
 
-    route(r'^$', name='counter', GET=show_counter, POST=inc_hangover_counter)
+    route(r'^$', name='counter', GET=show_counter, POST=inc_hangover_counter),
+
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}
+    )
 
 )
